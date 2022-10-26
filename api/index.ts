a82d287c43c433de4384713391a83f23a838515e
Amio Jin <amio.cn@gmail.com>
2022-10-26 22:07:05 +0800

    feat: add repository header

--- api/index.ts
+++ api/index.ts
@@ -1,5 +1,6 @@
 
 export default (req, res) => {
   res.setHeader('cache-control', 'public, s-maxage=31536000')
+  res.setHeader('repository', 'https://github.com/amio/echo-path.vercel.app')
   res.send(req.url)
 }
